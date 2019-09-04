5c0e9be9b7c287cd7b8ef1babfc82b35d9f1997e
magali <maguimar001@gmail.com>
2019-09-04 00:23:31 -0300

    profundizo un poco mas sobre el codigo, agrego comentarios

--- holaBot.js
+++ holaBot.js
@@ -1,6 +1,8 @@
-var builder = require('botbuilder');
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector);
+var builder = require('botbuilder'); // invocamos al bot builder -> requerimos el paquete
+var connector = new builder.ConsoleConnector().listen(); // el conector, nos permite unir consola con bot
+var bot = new builder.UniversalBot(connector); // bot que escucha consola
+
+// DEFINIMOS UN DIALOGO RAIZ ('/'), recibe un arreglo de funciones (cada funcion es una manera de interactuar con el usuario )
 bot.dialog('/',[
     function(session){
         session.send('Hola Mundo!');
